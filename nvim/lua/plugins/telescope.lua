c7a18e2808f870c7719dccf617456e9a6b56ce94
tomasmota <tomasrebelomota@gmail.com>
2024-01-16 10:49:30 +0100

    add telescope keymap for hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -69,6 +69,7 @@ return {
     -- keymaps
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>p', builtin.find_files)
+    vim.keymap.set('n', '<leader>a', function() builtin.find_files({hidden=true}) end)
     vim.keymap.set('n', '<leader>k', builtin.keymaps)
     vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols)
     vim.keymap.set('n', '<leader>fa', require('telescope').extensions.live_grep_args.live_grep_args)
