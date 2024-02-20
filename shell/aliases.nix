ea3370b0634f7508c4a6f1cf355438a01cc6ecba
tomasmota <tomasrebelomota@gmail.com>
2024-02-20 10:20:27 +0100

    fix kn alias

--- shell/aliases.nix
+++ shell/aliases.nix
@@ -22,7 +22,7 @@
   k = "kubectl";
   ks = ''kubectl config get-contexts -o name | fzf | xargs -I {} kubectl config use-context "{}"'';
   kc = ''kubectl config current-context'';
-  kn = ''kubectl get ns | awk '{print $1}' | fzf | xargs -I {} kubectl config set-context --current --namespace "{}"'';
+  kn = ''kubectl get ns --no-headers | awk '{print $1}' | fzf | xargs -I {} kubectl config set-context --current --namespace "{}"'';
   wclip = "/mnt/c/Windows/System32/clip.exe";
 
   # Git
