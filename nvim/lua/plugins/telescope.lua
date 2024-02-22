0884b20108fd74e106ac111046793aab20c6844d
tomasmota <tomasrebelomota@gmail.com>
2024-02-22 22:17:25 +0100

    improve telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,12 @@
 return {
   'nvim-telescope/telescope.nvim',
+  event = 'VeryLazy',
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-live-grep-args.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
-    'nvim-telescope/telescope-file-browser.nvim',
+    'nvim-telescope/telescope-frecency.nvim',
   },
   config = function()
     local telescope = require('telescope')
@@ -15,21 +16,9 @@ return {
       defaults = {
         path_display = {
           shorten = 20
-        },
-        mappings = {
-          i = {
-            ['<C-\\>'] = 'select_vertical',
-          },
-          n = {
-            ['<C-\\>'] = 'select_vertical',
-          }
         }
       },
       pickers = {
-        -- find_files = {
-        --     theme = "ivy",
-        --     layout_config = { height = 0.7 },
-        -- },
         lsp_document_symbols = {
           symbol_width = 70,
         }
@@ -43,42 +32,29 @@ return {
           auto_quoting = true,
           mappings = {
             i = {
-              ["<C-k>"] = lga_actions.quote_prompt({ postfix = " -t " }),           -- TODO: fix this
+              ["<C-k>"] = lga_actions.quote_prompt({ postfix = " -t " }),
             },
           },
         },
         ["ui-select"] = {
-          require("telescope.themes").get_dropdown({
-            previewer        = false,
-            initial_mode     = "normal",
-            sorting_strategy = 'ascending',
-            layout_strategy  = 'horizontal',
-            layout_config    = {
-              horizontal = {
-                width = 0.5,
-                height = 0.4,
-                preview_width = 0.6,
-              },
-            },
-          })
+          require("telescope.themes").get_dropdown {}
         },
       }
     })
 
-    telescope.load_extension('file_browser')
     telescope.load_extension('live_grep_args')
     telescope.load_extension('ui-select')
+    telescope.load_extension('frecency') --TODO: try this out
 
     -- keymaps
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>p', builtin.find_files)
-    vim.keymap.set('n', '<leader>a', function() builtin.find_files({hidden=true, no_ignore=true}) end)
+    vim.keymap.set('n', '<leader>a', function() builtin.find_files({ hidden = true, no_ignore = true }) end)
     vim.keymap.set('n', '<leader>k', builtin.keymaps)
     vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols)
     vim.keymap.set('n', '<leader>b', builtin.buffers)
     vim.keymap.set('n', '<leader>fa', require('telescope').extensions.live_grep_args.live_grep_args)
-    vim.keymap.set('n', '<leader>fb', require('telescope').extensions.file_browser.file_browser)
-    vim.keymap.set("n", "<leader>le", "<cmd>Telescope diagnostics<cr>")
+    vim.keymap.set("n", "<leader>le", builtin.diagnostics)
     vim.keymap.set('n', '<leader>fh', builtin.help_tags) -- grep help docs
     vim.keymap.set('n', '<leader>gst', builtin.git_status)
     vim.keymap.set('n', '<c-f>', builtin.current_buffer_fuzzy_find)
