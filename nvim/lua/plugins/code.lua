49cdfd96c565131e622072a8150f4ba051eec9d2
tomasmota <tomasrebelomota@gmail.com>
2024-03-23 16:23:15 +0100

    remove mini

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -43,12 +43,4 @@ return {
       })
     end,
   },
-  {
-    'echasnovski/mini.nvim',
-    config = function()
-      require('mini.ai').setup { n_lines = 500 }
-      require('mini.surround').setup()
-    end
-  },
-  'tpope/vim-sleuth',
 }
