ebc08b640ca90b3a9d6b8db8b45c1710feb366b0
tomasmota <tomasmota@hey.com>
2025-12-02 22:04:20 +0000

    fix ghostty after update

--- terminal/ghostty.nix
+++ terminal/ghostty.nix
@@ -1,9 +1,12 @@
-_:
-{
+_: {
   programs.ghostty = {
     enable = true;
+
     # install through package manager, configure through home-manager
     package = null;
+    systemd.enable = false;
+
+    enableZshIntegration = true;
     settings = {
       "theme" = "Catppuccin Mocha";
       "fullscreen" = true;
