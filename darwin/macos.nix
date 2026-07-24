687e54012b1eebc8a9f7350d0cabafad58cde772
tomasmota <tomasmota@hey.com>
2026-07-24 11:05:52 +0100

    run OpenChamber at login

--- darwin/macos.nix
+++ darwin/macos.nix
@@ -54,6 +54,36 @@
 
   services.tailscale.enable = true;
 
+  launchd.user.agents.openchamber = {
+    path = [
+      "/Users/tomas/.npm-global/bin"
+      "/etc/profiles/per-user/tomas/bin"
+      "/run/current-system/sw/bin"
+      "/usr/bin"
+      "/bin"
+      "/usr/sbin"
+      "/sbin"
+    ];
+    environment = {
+      HOME = "/Users/tomas";
+      OPENCODE_BINARY = "/Users/tomas/.npm-global/bin/opencode";
+      XDG_CONFIG_HOME = "/Users/tomas/.config";
+      XDG_DATA_HOME = "/Users/tomas/.local/share";
+    };
+    command = ''
+      ${pkgs.nodejs_24}/bin/node /Users/tomas/.npm-global/lib/node_modules/@openchamber/web/bin/cli.js serve --foreground --host 127.0.0.1 --port 3001
+    '';
+    serviceConfig = {
+      KeepAlive = true;
+      RunAtLoad = true;
+      ProcessType = "Background";
+      ThrottleInterval = 5;
+      WorkingDirectory = "/Users/tomas";
+      StandardOutPath = "/Users/tomas/Library/Logs/OpenChamber.log";
+      StandardErrorPath = "/Users/tomas/Library/Logs/OpenChamber.error.log";
+    };
+  };
+
   homebrew = {
     enable = true;
     casks = [
